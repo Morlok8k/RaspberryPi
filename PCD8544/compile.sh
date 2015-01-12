dea237ff65729ef2edc9f3881176a7c5c2d6f21f
Morlok8k <otis.spankmeyer@gmail.com>
2015-01-11 23:50:00 -0800

    copy/paste error fixed

--- PCD8544/compile.sh
+++ PCD8544/compile.sh
@@ -2,6 +2,6 @@
 
 gcc -o cpushow pcd8544_rpi.c PCD8544.c  -L/usr/local/lib -lwiringPi
 
-gcc -o test pcd8544_rpi.c PCD8544.c  -L/usr/local/lib -lwiringPi
+gcc -o test pcd8544_test.c PCD8544.c  -L/usr/local/lib -lwiringPi
 
-gcc -o test2 pcd8544_rpi.c PCD8544.c  -L/usr/local/lib -lwiringPi
+gcc -o test2 pcd8544_test2.c PCD8544.c  -L/usr/local/lib -lwiringPi
